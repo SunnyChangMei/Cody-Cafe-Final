7b26afb4930e4745b572c85f523bbe15003734fd
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-01 22:12:52 -0400

    complete coffee model

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,8 +1,39 @@
-const Sequelize = require('sequelize')
-const db = require('./database')
+const Sequelize = require('sequelize');
+const db = require('./database');
+const Op = Sequelize.Op;
 
 const Coffee = db.define('coffee', {
   // your code here
-})
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  ingredients: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  }
+});
 
-module.exports = Coffee
+Coffee.prototype.getIngredients = function() {
+  return this.ingredients.join(', ');
+};
+
+Coffee.findByIngredient = async function(thing) {
+  const ingredient = await Coffee.findAll({
+    where: {
+      ingredients: {
+        [Op.contains]: [thing]
+      }
+    }
+  });
+  return ingredient;
+};
+
+Coffee.beforeValidate(coffee => {
+  if (!coffee.ingredients) {
+    coffee.ingredients = ['love'];
+  } else if (!coffee.ingredients.includes('love')) {
+    coffee.ingredients.push('love');
+  }
+});
+
+module.exports = Coffee;
