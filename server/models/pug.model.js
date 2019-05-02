6b4002bb2eca97e90327066400b633f3a920cfb3
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-01 22:41:49 -0400

    complete pug.model

--- server/models/pug.model.js
+++ server/models/pug.model.js
@@ -18,4 +18,36 @@ const Pug = db.define('pugs', {
   }
 });
 
+Pug.prototype.isPuppy = function() {
+  return this.age < 1;
+};
+
+Pug.prototype.shortBio = function() {
+  let delimiter;
+  if (this.biography.indexOf('?') !== -1) delimiter = '?';
+  else if (this.biography.indexOf('!') !== -1) delimiter = '!';
+  else delimiter = '.';
+  return this.biography.split(delimiter)[0];
+};
+
+Pug.findByCoffee = async typeOfCoffee => {
+  const favoriteCoffee = await Pug.findAll({
+    include: [
+      {
+        model: Coffee,
+        as: 'favoriteCoffee',
+        where: {
+          name: typeOfCoffee
+        }
+      }
+    ]
+  });
+  return favoriteCoffee;
+};
+
+Pug.beforeValidate(pug => {
+  if (!pug.name) return;
+  pug.name = pug.name[0].toUpperCase() + pug.name.slice(1);
+});
+
 module.exports = Pug;
