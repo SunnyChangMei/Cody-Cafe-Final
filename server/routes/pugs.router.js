fd5992b59f156d6ac104118edd920041b8ff1d32
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-02 15:51:07 -0400

    complete pug routes

--- server/routes/pugs.router.js
+++ server/routes/pugs.router.js
@@ -4,5 +4,76 @@ const { Pug } = require('../models');
 // Your code here!
 // Remember that these routes are already mounted on
 // /api/pugs!
+router.get('/', async (req, res, next) => {
+  try {
+    res.status(200).send(await Pug.findAll());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:pugId', async (req, res, next) => {
+  try {
+    const pugId = req.params.pugId;
+    const foundId = await Pug.findById(pugId);
+    if (!foundId) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).send(foundId);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/favoriteCoffee/:favoriteCoffeeName', async (req, res, next) => {
+  try {
+    const favoriteCoffeeName = req.params.favoriteCoffeeName;
+    const findCoffee = await Pug.findByCoffee(favoriteCoffeeName);
+    res.status(200).send(findCoffee);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newPug = await Pug.create(req.body);
+    res.status(201).send(newPug);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:pugId', async (req, res, next) => {
+  try {
+    const pugId = req.params.pugId;
+    const thisPug = await Pug.findById(pugId);
+
+    if (!thisPug) {
+      res.sendStatus(404);
+    } else {
+      const updatePug = await thisPug.update(req.body);
+      res.status(200).send(updatePug);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:pugId', async (req, res, next) => {
+  try {
+    const pugId = req.params.pugId;
+    const thisPug = await Pug.findById(pugId);
+    if (!thisPug) {
+      res.sendStatus(404);
+    } else {
+      const deleteId = await thisPug.destroy(req.body);
+      res.status(204).send(deleteId);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
